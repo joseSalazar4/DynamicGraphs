5fba8d014110f5d53df504afb0b8f31523de05d2
Jose <mikomeka1@gmail.com>
2020-07-12 23:27:36 -0600

    Added main class
    This will likely change in the not so far future

---
+++ DynammicGraphs/src/main/java/Vertice.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mikom
+ */
+public class Vertice {
+    public static void main (String args[]){
+        System.out.println("We back in the game");
+        System.out.println("Go to bed");
+    }
+    
+}
+
